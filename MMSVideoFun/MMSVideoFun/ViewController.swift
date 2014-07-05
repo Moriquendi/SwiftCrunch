2a8cd0f68a97fcadeea526d7a64e675f7477d07d
Michal Smialko <michal.smialko@gmail.com>
2014-07-05 14:35:09 +0200

    Added new empty Xcode project.

---
+++ MMSVideoFun/MMSVideoFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MMSVideoFun
+//
+//  Created by Michal Smialko on 7/5/14.
+//  Copyright (c) 2014 NSSpot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
