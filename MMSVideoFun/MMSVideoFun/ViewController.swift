9a865eda9d3320c3491dcc6c256f1692fc0301b0
Michal Smialko <michal.smialko@gmail.com>
2014-07-06 12:10:31 +0200

    Ability to trim clips while merging.

--- MMSVideoFun/MMSVideoFun/ViewController.swift
+++ MMSVideoFun/MMSVideoFun/ViewController.swift
@@ -11,10 +11,17 @@ import AVFoundation
 import CoreMedia
 import MediaPlayer
 
-@infix func + (left: AVURLAsset, right: AVURLAsset) -> AVURLAsset! {
- 
-    // ------------------------------------------------- //
+@infix func + ( left:AVURLAsset,
+                right:(asset:AVURLAsset, range:Range<Int>)) -> AVURLAsset! {
+
+                    var fullRange:Range<Int> = Range(start: 0, end: Int(CMTimeGetSeconds(left.duration)))
+                    return (left, fullRange) + right
+}
+
+@infix func + ( left:(asset: AVURLAsset, range:Range<Int>),
+                right:(asset:AVURLAsset, range:Range<Int>)) -> AVURLAsset! {
     
+    // ------------------------------------------------- //
     func merge() -> AVURLAsset! {
         var composition = AVMutableComposition()
         
@@ -24,8 +31,9 @@ import MediaPlayer
         
         var t = kCMTimeZero;
         
-        let clips = [left.URL, right.URL]
-
+        let clips = [left.asset.URL, right.asset.URL]
+        let ranges = [left.range, right.range]
+        
         for i in 0..2 {
             let sourceAsset = AVURLAsset(URL: clips[i], options: [AVURLAssetPreferPreciseDurationAndTimingKey : true])
             
@@ -40,29 +48,35 @@ import MediaPlayer
          
             var error:NSErrorPointer = nil
             var ok = false;
-            ok = compositionVideoTrack.insertTimeRange(sourceVideoTrack.timeRange,
+            
+            let range = ranges[i]
+            var startSeconds:Float64 = Float64(range.startIndex)
+            var durationSeconds:Float64 = Float64(range.endIndex - range.startIndex)
+            
+            let timeRange:CMTimeRange = CMTimeRange(start: CMTimeMakeWithSeconds(startSeconds, 600),
+                                        duration: CMTimeMakeWithSeconds(durationSeconds, 600))
+            
+            
+            ok = compositionVideoTrack.insertTimeRange(timeRange,
                                         ofTrack: sourceVideoTrack,
                                         atTime: composition.duration,
                                         error: error)
-            
             if !ok {
                 NSLog("something went wrong");
             }
             
             // Audio
-            
             if sourceAsset.tracksWithMediaType(AVMediaTypeAudio).count > 0 {
                 let audioTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: CMPersistentTrackID(kCMPersistentTrackID_Invalid))
                 
-                audioTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero,
-                    sourceVideoTrack.timeRange.duration),
+                audioTrack.insertTimeRange(timeRange,
                     ofTrack: sourceAsset.tracksWithMediaType(AVMediaTypeAudio)[0] as AVAssetTrack,
                     atTime: t,
                     error: nil)
             }
             
 
-            t = CMTimeAdd(t, sourceVideoTrack.timeRange.duration);
+            t = CMTimeAdd(t, timeRange.duration);
         }
         
 
@@ -98,8 +112,8 @@ import MediaPlayer
             }
 
             // Remove old assetes
-            NSFileManager.defaultManager().removeItemAtURL(left.URL, error: nil)
-            NSFileManager.defaultManager().removeItemAtURL(right.URL, error: nil)
+            NSFileManager.defaultManager().removeItemAtURL(left.asset.URL, error: nil)
+            NSFileManager.defaultManager().removeItemAtURL(right.asset.URL, error: nil)
             
             println("FINISH")
             
@@ -108,6 +122,9 @@ import MediaPlayer
 
         // Wait for exporter to finish
         while !finished {}
+        
+        // Bleh, fuj. Exporter is deallocated. How to keep it?
+        println("\(exporter)")
         
         return AVURLAsset(URL: outputURL, options: nil)
     }
@@ -119,23 +136,16 @@ import MediaPlayer
 class ViewController: UIViewController {
     
     let player = MPMoviePlayerViewController()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    
+
     @IBAction func didTapButton(sender : UIButton) {
-        let clipURL1 = NSBundle.mainBundle().URLForResource("jamie1", withExtension: "mov");
-        let clipURL2 = NSBundle.mainBundle().URLForResource("jamie2", withExtension: "mov");
-        let clipURL3 = NSBundle.mainBundle().URLForResource("dave", withExtension: "mov");
+        var bundle = NSBundle.mainBundle()
         
-        let movie1 = AVURLAsset(URL: clipURL1, options: nil)
-        let movie2 = AVURLAsset(URL: clipURL2, options: nil)
-        let movie3 = AVURLAsset(URL: clipURL3, options: nil)
+        let movie1 = AVURLAsset(URL: bundle.URLForResource("jamie1", withExtension: "mov"), options: nil);
+        let movie2 = AVURLAsset(URL: bundle.URLForResource("jamie2", withExtension: "mov"), options: nil);
+        let movie3 = AVURLAsset(URL: bundle.URLForResource("dave", withExtension: "mov"), options: nil);
         
-        let asset:AVURLAsset = movie1 + movie2 + movie3
-        self.player.moviePlayer.contentURL = asset.URL
+        let mergedAsset:AVURLAsset = (movie1, 0..2) + (movie2, 0..2) + (movie3, 4..6)
+        self.player.moviePlayer.contentURL = mergedAsset.URL
     }
 
     @IBAction func play(sender : UIButton) {
